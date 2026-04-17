e5dfed85882e7f112f475e5341889785f4174bae
Ryan Ramdehol <ravinashwork@gmail.com>
2026-04-16 22:08:45 -0400

    project initialized

---
+++ main.py
@@ -0,0 +1,20 @@
+# ---------------------------- IMPORTS ------------------------------- #
+#
+# --------------.env file access flat string--------------#
+# Allows you to read the .env file
+# from dotenv import load_dotenv
+# import os
+# variable = os.getenv("<ENV VARIABLE>")
+# load_dotenv()
+# --------.env.json file access for JSON structure--------#
+# .env.json file
+# import json
+# with open('.env.json') as f:
+#     config = json.load(f)
+# value = config.get('YOUR_KEY')
+# ---------------------------- CONSTANTS ------------------------------- #
+# ---------------------------- GLOBAL VARIABLES ------------------------------- #
+
+# ---------------------------- FUNCTIONS ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
