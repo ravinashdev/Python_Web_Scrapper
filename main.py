a4bc5c6629b43d9ce72e979ac08c7891c9c81c4c
Ryan Ramdehol <ravinashwork@gmail.com>
2026-04-16 22:40:08 -0400

    playing around with beautiful soup features

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # ---------------------------- IMPORTS ------------------------------- #
-#
+from bs4 import BeautifulSoup
 # --------------.env file access flat string--------------#
 # Allows you to read the .env file
 # from dotenv import load_dotenv
@@ -13,8 +13,24 @@
 #     config = json.load(f)
 # value = config.get('YOUR_KEY')
 # ---------------------------- CONSTANTS ------------------------------- #
+# Parse the html file and parse its contents to set equal to a variable
+with open("website.html", "r") as file:
+    soup = BeautifulSoup(file, "html.parser")
 # ---------------------------- GLOBAL VARIABLES ------------------------------- #
 
 # ---------------------------- FUNCTIONS ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+
+print(soup.prettify())
+# Dictionary key:value notation to read a webpage
+print(soup.title)
+print(soup.html.head.meta)
+print(soup.h3["class"])
+
+# Grab all URLS on a website
+for link in soup.find_all('a'):
+    print(link.get('href'))
+
+print(soup.get_text())
