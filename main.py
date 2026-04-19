72adce8b60c5c70f47920d764d79b4729efa431b
Ryan Ramdehol <ravinashwork@gmail.com>
2026-04-19 16:17:59 -0400

    scrape hacker news for Titles and Link

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # ---------------------------- IMPORTS ------------------------------- #
 from bs4 import BeautifulSoup
+import requests
 # --------------.env file access flat string--------------#
 # Allows you to read the .env file
 # from dotenv import load_dotenv
@@ -12,36 +13,27 @@ from bs4 import BeautifulSoup
 # with open('.env.json') as f:
 #     config = json.load(f)
 # value = config.get('YOUR_KEY')
+# --------DYNACONF Config Prod & Dev--------#
 # ---------------------------- CONSTANTS ------------------------------- #
-# Parse the html file and parse its contents to set equal to a variable
-with open("website.html", "r") as file:
-    soup = BeautifulSoup(file, "html.parser")
+
 # ---------------------------- GLOBAL VARIABLES ------------------------------- #
 
 # ---------------------------- FUNCTIONS ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+url_response = requests.get("https://news.ycombinator.com/news")
+# print(response.text)
+soup = BeautifulSoup(url_response.text, "html.parser")
+# print(soup.prettify())
+titles = soup.find_all("td", class_="title")
+title_text = titles
+for element in titles:
+    # print(element)
+    anchor = element.find("a")
+    # print(anchor)
+    if anchor:
+        anchor_text = anchor.get_text(strip=True)
+        anchor_link = anchor["href"]
+        print(anchor_text)
+        print(anchor_link)
 
-
-print(soup.prettify())
-# Dictionary key:value notation to read a webpage
-print(soup.title)
-print(soup.html.head.meta)
-print(soup.h3["class"])
-
-# Grab all URLS on a website
-for link in soup.find_all('a'):
-    print(link.get('href'))
-# Get all text
-print(soup.get_text())
-# Get element with a **Kwargs
-print(soup.find(name="h1", id="name"))
-print(soup.find(name="h3", class_="heading"))
-# Get element with CSS selectors
-my_website = (soup.select_one("p a"))["href"]
-print(my_website)
-# print(my_website["href"])
-# Get element with ID name using #
-print(soup.select_one(selector="#name"))
-# Select by class of Heading
-print(soup.select_one(selector=".heading"))
---
+++ scratch.py
@@ -0,0 +1,47 @@
+# ---------------------------- IMPORTS ------------------------------- #
+from bs4 import BeautifulSoup
+# --------------.env file access flat string--------------#
+# Allows you to read the .env file
+# from dotenv import load_dotenv
+# import os
+# variable = os.getenv("<ENV VARIABLE>")
+# load_dotenv()
+# --------.env.json file access for JSON structure--------#
+# .env.json file
+# import json
+# with open('.env.json') as f:
+#     config = json.load(f)
+# value = config.get('YOUR_KEY')
+# ---------------------------- CONSTANTS ------------------------------- #
+# Parse the html file and parse its contents to set equal to a variable
+with open("website.html", "r") as file:
+    soup = BeautifulSoup(file, "html.parser")
+# ---------------------------- GLOBAL VARIABLES ------------------------------- #
+
+# ---------------------------- FUNCTIONS ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+print(soup.prettify())
+# Dictionary key:value notation to read a webpage
+print(soup.title)
+print(soup.html.head.meta)
+print(soup.h3["class"])
+
+# Grab all URLS on a website
+for link in soup.find_all('a'):
+    print(link.get('href'))
+# Get all text
+print(soup.get_text())
+# Get element with a **Kwargs
+print(soup.find(name="h1", id="name"))
+print(soup.find(name="h3", class_="heading"))
+# Get element with CSS selectors
+my_website = (soup.select_one("p a"))["href"]
+print(my_website)
+# print(my_website["href"])
+# Get element with ID name using #
+print(soup.select_one(selector="#name"))
+# Select by class of Heading
+print(soup.select_one(selector=".heading"))
