3829d28fba14599e1571e4837f5a9d99ed0e59c8
Ryan Ramdehol <ravinashwork@gmail.com>
2026-04-19 18:50:24 -0400

    scraped atricles and put them in a dict of ascending and descending order to display list by ranking score

--- main.py
+++ main.py
@@ -25,15 +25,56 @@ url_response = requests.get("https://news.ycombinator.com/news")
 # print(response.text)
 soup = BeautifulSoup(url_response.text, "html.parser")
 # print(soup.prettify())
-titles = soup.find_all("td", class_="title")
-title_text = titles
-for element in titles:
-    # print(element)
-    anchor = element.find("a")
-    # print(anchor)
-    if anchor:
-        anchor_text = anchor.get_text(strip=True)
-        anchor_link = anchor["href"]
-        print(anchor_text)
-        print(anchor_link)
+# title_row_1 = soup.find(name="tr", class_="athing submission").find_all("a")[1]
+# title_row_2 = soup.find(name="td", class_="subtext").find("span", class_="score")
+title_row_1 = soup.find_all(name="tr", class_="athing submission")
+title_row_2 = soup.find_all(name="td", class_="subtext")
+
+
+# print(title_row_1)
+# print(title_row_2)
+articles = []
+# Use a zip sort to iterate over two list since there is a relation
+for anchor_row, score_row in zip(title_row_1, title_row_2):
+    # print(anchor_row.find_all("a")[1].get_text(strip=True))
+    # print(anchor_row.find_all("a")[1].get("href"))
+    # print(score_row.find("span", class_="score").get_text(strip=True))
+    # print("\n")
+    new_entry= {
+        "title": anchor_row.find_all("a")[1].get_text(strip=True),
+        "link": anchor_row.find_all("a")[1].get("href"),
+        # Split string into elements and convert it to an into to later sort the dict
+        "score": int(score_row.find_all("span")[1].get_text(strip=True).split()[0]),
+    }
+    articles.append(new_entry)
+# print(articles)
+# List to sort, key to sort by with an anonymous function, and the direction of the sort
+# from collections import OrderedDict
+# sorted_ordered = OrderedDict(sorted(d.items(), key=lambda item: item[1]))
+sorted_articles_descending_by_score = sorted(articles, key=lambda k: k["score"], reverse=True)
+sorted_articles_ascending_by_score = sorted(articles, key=lambda k: k["score"], reverse=False)
+
+print("Articles Descending")
+for article in sorted_articles_descending_by_score:
+    title = article["title"]
+    link = article["link"]
+    score = article["score"]
+    print(f"Title: {title} \nLink: {link} \nScore: {score}")
+    print("\n")
+
+print("Articles Ascending")
+for article in sorted_articles_ascending_by_score:
+    title = article["title"]
+    link = article["link"]
+    score = article["score"]
+    print(f"Title: {title} \nLink: {link} \nScore: {score}")
+    print("\n")
+
+
+
+
+
+
+
+
 
